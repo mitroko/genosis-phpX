6147ebcae664d7c0af978171108fa9740393f0a3
Dzmitry Stremkouski <dstremkouski@mirantis.com>
2021-03-23 00:53:52 +0100

    docker compose tree

---
+++ docker/config/config.php
@@ -0,0 +1,7 @@
+<?php
+$_conf['jrCore_db_host'] = '127.0.0.1';
+$_conf['jrCore_db_port'] = '3306';
+$_conf['jrCore_db_name'] = 'genosis';
+$_conf['jrCore_db_user'] = 'genosis';
+$_conf['jrCore_db_pass'] = 'genosis_user_password_here';
+$_conf['jrCore_base_url'] = 'http://127.0.0.1:8083';
